0ba7aa35ed101b8eda386604601a7954c16d769f
gopal das <gopal1409@gmail.com>
2020-08-28 01:54:41 -0600

    azure network interface

--- main.tf
+++ main.tf
@@ -21,3 +21,14 @@ resource "azurerm_subnet" "web_server_subnet" {
     address_prefix = var.web_server_address_prefix
 
 }
+
+resource "azurerm_network_interface" "web_server_nic" {
+    name = "${var.web_server_name}-nic"
+    location = var.web_server_location
+    resource_group_name = azurerm_resource_group.web_server_rg.name   
+    ip_configuration {
+    name                          = "${var.web_server_name}-ip"
+    subnet_id                     = azurerm_subnet.web_server_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
